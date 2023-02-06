99703b38b3164f598d89ff567d1491b4d8195ceb
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2023-02-06 20:49:24 +0100

    react views

--- app/javascript/components/Listing.jsx
+++ app/javascript/components/Listing.jsx
@@ -18,23 +18,54 @@ const Listing = () => {
       .catch(() => navigate("/"));
   }, []);
 
-  // const allListings = listings.map((listing, index) => () => (
-    
-//   return (
-//     <div>
-//       {listings.map((listing) => (
-//         <div key={listing.id} class="card" style="width: 18rem;">
-//           <img src={listing.image} class="card-img-top" alt="..." />
-//           <div class="card-body">
-//             <h5 class="card-title">{listing.description}</h5>
-//             <p class="card-text">{listing.location}</p>
-//             <a href="#" class="btn btn-primary">
-//               Go somewhere
-//             </a>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
+  const allListings = listings.map((listing, index) =>  (
+    <div key={index} className="card" style={{width: '18rem'}}>
+      <img src={listing.image} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{listing.description}</h5>
+        <p className="card-text">{listing.location}</p>
+        <Link to={`/listing/${listing.id}`} className="btn custom-button">
+          View Listing
+        </Link>
+      </div>{" "}
+    </div>
+  ));
+  const noListing = (
+    <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
+      <h4>
+        No listings yet. Why not <Link to="/new_listing">create one</Link>
+      </h4>
+    </div>
+  );
+
+  return (
+    <>
+      <section className="jumbotron jumbotron-fluid text-center">
+        <div className="container py-5">
+          <h1 className="display-4">Listing</h1>
+          <p className="lead text-muted">
+            We’ve pulled together our most popular listings, our latest
+            additions, and our editor’s picks, so there’s sure to be something
+            tempting for you.
+          </p>
+        </div>
+      </section>
+      <div className="py-5">
+        <main className="container">
+          <div className="text-right mb-3">
+            <Link to="/listing" className="btn custom-button">
+              Create New Listing
+            </Link>
+          </div>
+          <div className="row">
+            {listings.length > 0 ? allListings : noListing}
+          </div>
+          <Link to="/" className="btn btn-link">
+            Home
+          </Link>
+        </main>
+      </div>
+    </>
+  );
 };
 export default Listing;
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
-
+import Listing from "../components/Listing";
 export default (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/listing" element={<Listing />} />
     </Routes>
   </Router>
 );
