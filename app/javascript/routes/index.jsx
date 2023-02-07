86a90ca18e5fefa5b227c2508d9d607bb91b1c9e
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2023-02-07 16:05:19 +0100

    single listing

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Listings from "../components/Listings";
+import Listing from "../components/Listing";
+
 export default (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/listing" element={<Listings />} />
+      <Route path="/listing/:id" element={<Listing />} />
     </Routes>
   </Router>
 );
