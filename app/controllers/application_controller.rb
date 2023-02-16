b11e6353a6ec21f7066140b0ab3a5e8b4ec3766c
momoelabdi <momo.elabdi@bluewin.ch>
2023-02-16 17:41:10 +0100

    checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
     respond_to :json
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-   def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-   end
+    def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 
     def after_sign_in_path_for(resource)
         request.env['omniauth.origin'] || stored_location_for(resource) || root_path
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_121331) do
     t.string "name", null: false
     t.text "location", null: false
     t.text "description", null: false
-    t.string "image", default: "https://images.unsplash.com/photo-1542732270-4e6a7a68d8d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    t.string "image", default: "https://upload.wikimedia.org/wikipedia/commons/7/71/Photo_of_image_saab.png"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
